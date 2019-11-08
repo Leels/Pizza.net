dad21cf8de7cb55a2a7e66d9187e7ddd4abd06a9
Lela Smith <smith.lelaa@gmail.com>
2019-11-08 09:09:42 -0800

    create pizza order constructor in js.

---
+++ js/scrpits.js
@@ -0,0 +1,10 @@
+function PizzaOrder(size, crust) {
+  this.size = size,
+  this.crust = crust,
+  this.beverages = [],
+  this.toppings = []
+}
+
+PizzaOrder.prototype.orderPrice = function() {
+  
+}
