f8007e6d8b9417a06e17da463c7551b40b1ac132
Lela Smith <smith.lelaa@gmail.com>
2019-11-08 10:49:41 -0800

    add front-end inputs to front-end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -43,3 +43,27 @@ PizzaOrder.prototype.orderPrice = function() {
   };
   return this.price.toFixed(2);
 }
+
+//Front-end logic for pizza order
+
+$(document).ready(function(){
+  $("form#pizza-order").submit(function(event){
+    event.preventDefault();
+
+    console.log("inputBeverages");
+    // var inputSize =
+    // var inputCrust =
+    var inputBeverages = $('input:checkbox[name=beverages]:checked').each(function() {
+      return $(this).val();});
+    var inputMeatToppings = $('input:checkbox[name=meats]:checked').map(function() {
+      return this.value;}).get();
+    var inputVeggieToppings = $('input:checkbox[name=veggies]:checked').map(function() {
+      return this.value;}).get();
+    var newPizzaOrder = new PizzaOrder(inputSize, inputCrust, inputBeverages, inputMeatToppings, inputVeggieToppings);
+
+    newPizzaOrder.orderPrice();
+
+    var totalPrice = newPizzaOrder.orderPrice();
+    $("#total").text(totalPrice);
+  });
+});
