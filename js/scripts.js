e95977434eadbcaa9145a18d024ff8670202b7c7
Lela Smith <smith.lelaa@gmail.com>
2020-11-11 21:30:44 -0800

    Fix project grading example

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//Back-end logic for pizza order//
+//Back-end logic for pizza order
 
 function PizzaOrder(size, crust, beverages, combo, meatToppings, veggieToppings) {
   this.size = size,
@@ -11,13 +11,13 @@ function PizzaOrder(size, crust, beverages, combo, meatToppings, veggieToppings)
 }
 
 PizzaOrder.prototype.orderPrice = function() {
-  if (this.size === "small") {
+  if (this.size === "Small") {
     this.price += 8;
-  } else if (this.size === "medium") {
+  } else if (this.size === "Medium") {
     this.price +=10;
-  } else if (this.size === "large") {
+  } else if (this.size === "Large") {
     this.price +=12;
-  } else if (this.size === "x-large") {
+  } else if (this.size === "X-large") {
     this.price +=14;
   }
 
@@ -46,63 +46,67 @@ PizzaOrder.prototype.orderPrice = function() {
   return this.price.toFixed(2);
 }
 
-
-
-//Front End logic for customer information form//
-
+//Front End logic for customer information form
 $(document).ready(function(){
   $("form#customer-info").submit(function(event){
     event.preventDefault();
 
-    var nameInput = $("input#customerName").val();
-    var pickUpDelivery = $('input[name=delivery]:checked').val();
+    let nameInput = $("input#customerName").val();
+    let pickUpDelivery = $('input[name=delivery]:checked').val();
 
     $("#customer-info").hide();
     $("#pizza-order").slideDown("slow");
     $(".name").text(nameInput).val();
 
-    //Front-end logic for pizza order//
-
+    //Front-end logic for pizza order
     $(document).ready(function(){
       $("form#pizza-order").submit(function(event){
         event.preventDefault();
 
-        var inputSize = $('input[name=size]:checked').val();
-        var inputCrust = $('input[name=crust]:checked').val();
-        var inputBeverages = $('input:checkbox[name=beverages]:checked').map(function() {
+        let inputSize = $('input[name=size]:checked').val();
+        let inputCrust = $('input[name=crust]:checked').val();
+        let inputBeverages = $('input:checkbox[name=beverages]:checked').map(function() {
           return this.value;}).get();
-          var inputCombo = $('input[name=combo]:checked').val();
-          var inputMeatToppings = $('input:checkbox[name=meats]:checked').map(function() {
-            return this.value;}).get();
-            var inputVeggieToppings = $('input:checkbox[name=veggies]:checked').map(function() {
-              return this.value;}).get();
-              var newPizzaOrder = new PizzaOrder(inputSize, inputCrust, inputBeverages, inputCombo, inputMeatToppings, inputVeggieToppings);
-              var totalPrice = newPizzaOrder.orderPrice();
+        let inputCombo = $('input[name=combo]:checked').val();
+        let inputMeatToppings = $('input:checkbox[name=meats]:checked').map(function() {
+          return this.value;}).get();
+        let inputVeggieToppings = $('input:checkbox[name=veggies]:checked').map(function() {
+          return this.value;}).get();
+        let newPizzaOrder = new PizzaOrder(inputSize, inputCrust, inputBeverages, inputCombo, inputMeatToppings, inputVeggieToppings);
+        let totalPrice = newPizzaOrder.orderPrice();
 
-              $("#orderSize").text(inputSize);
-              $("#orderCrust").text(inputCrust);
-              $("#orderToppings").text(inputCombo + inputMeatToppings + inputVeggieToppings);
-              $("#orderBeverages").text(inputBeverages);
-              $("#orderPrice").text("$" + totalPrice);
+        $("#orderSizeCrust").text(inputSize + " " + inputCrust);
+        
+        if (inputCombo) {
+          $("#orderCombo").text(inputCombo + ".");
+        }
+        if (inputMeatToppings) {
+          $("#orderMeatToppings").text(inputMeatToppings);
+        }
+        if (inputVeggieToppings) {
+          $("#orderVeggieToppings").text(inputVeggieToppings);
+        }
+        if (inputBeverages) {
+          $("#orderBeverages").text(" " + inputBeverages);
+        }
+        $("#orderPrice").text("$" + totalPrice);
+        $("#confirmation").show();
+        $(".submit-btn").hide();
+        $("#initially-hidden").show();
 
-              $("#confirmation").show();
-              $(".submit-btn").hide();
-              $("#initially-hidden").show();
+        if (pickUpDelivery === "delivery") {
+          $(".delivery-confirmation").show();
+        } else if (pickUpDelivery === "pick-up") {
+          $(".pickup-confirmation").show();
+        }
 
-              if (pickUpDelivery === "delivery") {
-                $(".delivery-confirmation").show();
-              } else if (pickUpDelivery === "pick-up") {
-                $(".pickup-confirmation").show();
-              }
-
-              $(".send-order-btn").click(function(){
-                $("#pizza-order").hide();
-                $(".final").slideDown("slow");
-
-              });
-
-            });
-          });
-
+        $(".send-order-btn").click(function(){
+          $("#pizza-order").hide();
+          $(".final").slideDown("slow");
         });
+
       });
+    });
+
+  });
+});
